3734ec3b84547462962438ba068d3ee8d0e5fb26
Benedict Issah <benedictissah@gmail.com>
2024-06-28 13:57:45 -0500

    Signup selected element updated

--- src/Components/Home/Signup.js
+++ src/Components/Home/Signup.js
@@ -149,7 +149,8 @@ function Signup({ setStatus }) {
                   Role
                 </label>
                 <select
-                  value={"voter"}
+                  defaultValue={"voter"}
+                  multiple={true}
                   onChange={(e) => setRole(e.target.value)}
                   className="  divide-y w-full content-center text-base py-1 border-b border-gray-300 focus:outline-none focus:border-indigo-500"
                 >
